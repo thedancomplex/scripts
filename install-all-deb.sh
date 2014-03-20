2fee6c3e918f301dddbcd028477fffee0cc952cb
Daniel M. Lofaro <dan@danlofaro.com>
2014-03-20 12:27:25 -0700

    added install root

--- install-all-deb.sh
+++ install-all-deb.sh
@@ -1,4 +1,8 @@
-sudo ls >> /dev/null
+sudo ls > /dev/null
+INSTALL_ROOT=$('pwd')
 . ./install-utils.sh
+cd $INSTALL_ROOT
 . ./install-hubo-ach.sh
+cd $INSTALL_ROOT
 . ./install-openrave.sh
+cd $INSTALL_ROOT
