28e60d24415c4a82ce3e6fb47ba260f5e5b1781d
Daniel Lofaro <geovana@daniels-mbp.vsnet.gmu.edu>
2014-03-20 14:46:15 -0400

    added install for utils

---
+++ install-hubo-ach.sh
@@ -0,0 +1,6 @@
+cd ~/
+mkdir projects
+cd projects
+git clone https://github.com/hubo/hubo-ach
+cd hubo-ach
+./first-time-install-hubo-ach.sh
---
+++ install-utils.sh
@@ -0,0 +1,3 @@
+sudo apt-get install openssh-server
+sudo apt-get install vim
+sudo apt-get install git-core
