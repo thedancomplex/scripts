d065373ccef134a85a1d30345cb69fd62defa51d
Daniel Lofaro <dan@danLofaro.com>
2019-02-07 17:30:16 -0500

    added install brew and hopefully fixed add tab complete

--- addTabComplete.sh
+++ addTabComplete.sh
@@ -1,3 +1,4 @@
-echo 'set completion-ignore-case on' >> ~/.inputrc
-echo 'set show-all-if-ambiguous on' >> ~/.inputrc
-echo 'TAB: menu-complete' >> ~/.inputrc
+brew install zsh
+#echo 'set completion-ignore-case on' >> ~/.inputrc
+#echo 'set show-all-if-ambiguous on' >> ~/.inputrc
+#echo 'TAB: menu-complete' >> ~/.inputrc
