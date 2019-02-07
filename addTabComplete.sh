67ba3e1a3ebadbe5b9f87038f11a4488ce21ba89
Daniel Lofaro <dan@danLofaro.com>
2019-02-07 17:45:09 -0500

    still need to add tab complete

--- addTabComplete.sh
+++ addTabComplete.sh
@@ -1,4 +1,10 @@
-brew install zsh
+#brew install zsh
+#brew install git bash-completion
+curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o ~/.git-completion.bash
+echo 'if [ -f ~/.git-completion.bash ]; then
+  . ~/.git-completion.bash
+fi' >> ~/.bash_profile
+#echo '[ -f /usr/local/etc/bash_completion ] && . /usr/local/etc/bash_completion' >> ~/.bash_profile
 #echo 'set completion-ignore-case on' >> ~/.inputrc
 #echo 'set show-all-if-ambiguous on' >> ~/.inputrc
 #echo 'TAB: menu-complete' >> ~/.inputrc
